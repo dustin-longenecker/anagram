544a354076f51969325e9b0a0f09cf542ebef9d3
dustin-longenecker <dl-dev@outlook.com>
2020-03-11 12:05:14 -0600

    init commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+gem 'json'
+gem 'activerecord'
+gem 'pg'
+gem 'puma'
+gem 'rack'
+gem 'rack-test'
+gem 'rake'
+gem 'rspec'
+gem 'sinatra'
+gem 'sinatra-activerecord'
---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,22 @@
+get '/' do
+  erb :index
+end
+
+get '/anagrams/:word' do
+  @word = params[:word]
+  @anagrams = Word.find_anagrams(@word)
+  erb :show
+end
+
+post '/' do
+  @word = params[:word]
+  redirect "/anagrams/#{@word}"
+  if Word.valid_word(word)
+    anagram_array = Person.get_birth_path_num(birthdate)
+    redirect "/anagram/#{word}"
+  else
+    @error = "You should enter a valid word containing only 3 letters."
+    erb :form
+  end
+
+end
